1050ce19947f14e915eff65566987cf6881a2bde
KrzysztofSym <krzysztof.symoniuk@gmail.com>
2025-03-16 22:40:07 +0100

    Second query resolved

--- mysql_orders.sql
+++ mysql_orders.sql
@@ -1,3 +1,4 @@
+-- CREATE DATABASE db_orders;
 USE db_orders;
 -- CREATE TABLE df_orders (
 --   order_id INT PRIMARY KEY,
@@ -25,4 +26,14 @@ SELECT product_id, SUM(sale_price * quantity) AS revenue FROM df_orders
 GROUP BY product_id
 ORDER BY revenue DESC
 LIMIT 10;
-
+-- Find top 5 highest selling products in each region
+WITH cte AS
+(SELECT region, product_id, SUM(sale_price) AS sales FROM df_orders
+GROUP BY 1, 2)
+SELECT * FROM
+(SELECT *, row_number() OVER (PARTITION BY region ORDER BY sales DESC) AS rn
+FROM cte) A
+WHERE rn <= 5;
+-- Find month over month growth comparison for 2022 and 2023 sales eg: jan 2022 vs jan 2023
+SELECT year(order_date), month(order_date), SUM(sale_price) FROM df_orders
+GROUP BY 1, 2;
